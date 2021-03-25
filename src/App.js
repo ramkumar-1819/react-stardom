faaa58bae48e9ae2364d6affd3d7d9bec26c492a
ramkumar-1819 <rohitram1819@gmail.com>
2021-03-25 14:42:51 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Display from './Display/DisplayComponent'
 
 function App() {
   return (<div>
-          <div className="title">PROSTARSs</div>
+          <div className="title">PROSTARS</div>
           <Display/>   
           </div>);             //Display component hold the details of these stars
 }
